44173bf2fb430b800665a145ac2450c529ae379b
Sonja Popovic <cod55010@adobe.com>
2025-10-06 14:25:01 +0200

    MWPW-181343-initial

--- eds/blocks/training-preview/TrainingPreview.js
+++ eds/blocks/training-preview/TrainingPreview.js
@@ -1,6 +1,6 @@
 import { getLibs } from '../../scripts/utils.js';
 import { trainingPreviewStyles } from './TrainingPreviewStyles.js';
-
+// test page http://localhost:3000/digitalexperience/drafts/sonja/trainings/preview
 const miloLibs = getLibs();
 const { html, LitElement } = await import(`${miloLibs}/deps/lit-all.min.js`);
 export default class TrainingPreview extends LitElement {
@@ -15,6 +15,26 @@ export default class TrainingPreview extends LitElement {
     super();
   }
 
+  firstUpdated() {
+    // Create container for React to mount into
+    const container = document.createElement('div');
+    container.id = 'root_content_outer_position_component';
+    this.appendChild(container);
+
+    // Inject the CSS file (if not already added globally)
+    const cssLink = document.createElement('link');
+    cssLink.rel = 'stylesheet';
+    cssLink.href = '/eds/blocks/training-preview/dist/css/cptraining.min.css'; // adjust path
+    document.head.appendChild(cssLink);
+
+    // Load the React bundle script
+    const script = document.createElement('script');
+    script.src = '/eds/blocks/training-preview/dist/js/cptraining.min.js'; // adjust path
+    script.type = 'text/javascript';
+    script.async = true;
+    this.appendChild(script);
+  }
+
   async connectedCallback() {
     super.connectedCallback();
     // fetch if needed
@@ -22,6 +42,6 @@ export default class TrainingPreview extends LitElement {
 
   // eslint-disable-next-line class-methods-use-this
   render() {
-    return html`<div class="training-preview-container"></div> `;
+    return html`<div class="training-preview-container">test </div> `;
   }
 }

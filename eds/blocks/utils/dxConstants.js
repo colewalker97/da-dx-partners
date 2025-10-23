348ff563614e3f5bf8328ff16983920866cbbe33
Ben Zahler <ben@hitthecode.com>
2025-10-23 14:31:33 +0200

    MWPW-177330: update metadata url to new structure

--- eds/blocks/utils/dxConstants.js
+++ eds/blocks/utils/dxConstants.js
@@ -2,10 +2,10 @@ import { getPartnerDataCookieValue} from '../../scripts/utils.js';
 
 export const RT_SEARCH_ACTION_PATH = '/api/v1/web/da-dx-partners-runtime/search-apc/search-apc?';
 
-export const DX_PROGRAM_TYPE = 'spp';
+export const DX_PROGRAM_TYPE = 'DXP';
 
 export const PARTNER_LEVEL = getPartnerDataCookieValue('level', DX_PROGRAM_TYPE);
 
 export const DIGITALEXPERIENCE_PREVIEW_PATH = '/digitalexperience/preview/';
 export const DIGITALEXPERIENCE_ASSETS_PATH = '/digitalexperience-assets/';
-export const PX_ASSETS_AEM_PATH = '/content/dam/dxp/';
+export const PX_ASSETS_PREVIEW_PATH = '/digitalexperience-assets/preview/';
